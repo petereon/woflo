3b3434df2d02b24947abd44dc482925ab3e42f92
petereon <pvyboch1@gmail.com>
2022-09-24 14:46:15 +0200

    docs: updating example

--- example.py
+++ example.py
@@ -5,14 +5,14 @@ from woflo import task
 
 
 @task(retries=2, retry_wait_time=2)
-def sleepy_sum(a, b):
+def sleepy_sum(a: int, b: int) -> int:
     time.sleep(a + b)
     if randint(1, 5) > 2:
         raise Exception('Failed')
     return a + b
 
 
-def workflow(sleepers: int = 5):
+def workflow(sleepers: int = 5) -> None:
     task_runs = []
     for i in range(sleepers):
         task_runs.append(sleepy_sum(i, 2))
